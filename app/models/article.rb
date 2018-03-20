63d8e8ff164bad6778bdf5f808f58eff3c9f63e0
Arun VK <arun_bolt4@yahoo.in>
2018-03-20 17:07:19 +0000

    final commit

---
+++ app/models/article.rb
@@ -0,0 +1,10 @@
+class Article < ActiveRecord::Base
+    belongs_to :user
+    belongs_to :category
+    
+    
+    def self.search(query)
+        where("title like ?", "%#{query}%") 
+    end
+    
+end
